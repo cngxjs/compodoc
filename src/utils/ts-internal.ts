59f111caba701251dae7a5c6cd62283a3de6ab93
Tomas Corral Casas <tomascorralcasas@Mac.home>
2025-07-14 20:33:06 +0200

    Revert linting issues in src/utils/ts-internal.ts

--- src/utils/ts-internal.ts
+++ src/utils/ts-internal.ts
@@ -2,11 +2,11 @@ import * as ts from 'typescript';
 const tsany = ts as any;
 
 // https://github.com/Microsoft/TypeScript/blob/v2.1.4/src/compiler/utilities.ts#L152
-export function getSourceFileOfNode(...args: any[]): ts.SourceFile {
-    return tsany.getSourceFileOfNode.apply(this, args);
+export function getSourceFileOfNode(node: ts.Node): ts.SourceFile {
+    return tsany.getSourceFileOfNode.apply(this, arguments);
 }
 
 // https://github.com/Microsoft/TypeScript/blob/v2.1.4/src/compiler/utilities.ts#L1423
-export function getJSDocCommentRanges(...args: any[]) {
-    return tsany.getJSDocCommentRanges.apply(this, args);
+export function getJSDocCommentRanges(node: ts.Node, text: string) {
+    return tsany.getJSDocCommentRanges.apply(this, arguments);
 }
