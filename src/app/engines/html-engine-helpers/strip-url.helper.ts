1b6ba2d4f456e9bd48bdbdb92f918bb67267666e
Tomas Corral Casas <tomascorralcasas@Mac.home>
2025-07-14 19:47:56 +0200

    Revert linting issues in src/app/engines/html-engine-helpers/strip-url.helper.ts

--- src/app/engines/html-engine-helpers/strip-url.helper.ts
+++ src/app/engines/html-engine-helpers/strip-url.helper.ts
@@ -2,6 +2,6 @@ import { IHtmlEngineHelper } from './html-engine-helper.interface';
 
 export class StripURLHelper implements IHtmlEngineHelper {
     public helperFunc(context: any, prefix: string, url: string, options): string {
-        return prefix + url.split('/').pop();
+        return prefix + url.split("/").pop();
     }
 }
