1b9d30ff8a6138b2819026aac31ea07660be81a2
Tomas Corral Casas <tomascorralcasas@Mac.home>
2025-07-14 19:55:41 +0200

    Remove linting issue in src/utils/get-module-with-providers.ts

--- src/utils/get-module-with-providers.ts
+++ src/utils/get-module-with-providers.ts
@@ -4,16 +4,16 @@ export function getModuleWithProviders(node: ts.VariableStatement) {
     let result;
     if (node.declarationList) {
         if (node.declarationList.declarations && node.declarationList.declarations.length > 0) {
-            let i = 0;
-            const len = node.declarationList.declarations.length;
+            let i = 0,
+                len = node.declarationList.declarations.length;
 
             for (i; i < len; i++) {
-                const declaration = node.declarationList.declarations[i];
+                let declaration = node.declarationList.declarations[i];
 
                 if (declaration.type) {
-                    const type: ts.TypeReferenceNode = declaration.type as ts.TypeReferenceNode;
+                    let type: ts.TypeReferenceNode = declaration.type as ts.TypeReferenceNode;
                     if (type.typeName) {
-                        const text = type.typeName.getText();
+                        let text = type.typeName.getText();
                         if (text === 'ModuleWithProviders') {
                             result = declaration.initializer;
                         }
