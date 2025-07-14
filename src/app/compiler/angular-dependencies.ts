54baa409607d761bc858cd980460dc016ff51f8a
Tomas Corral Casas <tomascorralcasas@Mac.home>
2025-07-14 19:36:32 +0200

    Rever linting issues in src/app/compiler/angular-dependencies.ts

--- src/app/compiler/angular-dependencies.ts
+++ src/app/compiler/angular-dependencies.ts
@@ -255,7 +255,7 @@ export class AngularDependencies extends FrameworkDependencies {
         deps.inputsClass = IO.inputs ?? [];
         deps.outputsClass = IO.outputs ?? [];
         if (IO.properties) {
-            const { inputSignals, outputSignals, properties } = this.componentHelper.getInputOutputSignals(IO.properties);
+            const {inputSignals, outputSignals, properties} = this.componentHelper.getInputOutputSignals(IO.properties);
 
             deps.inputsClass = deps.inputsClass.concat(inputSignals)
             deps.outputsClass = deps.outputsClass.concat(outputSignals)
