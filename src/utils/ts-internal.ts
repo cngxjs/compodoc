9f412173e1c2beb8176b61f0c0031f20d4eb1fcb
tcorral <amischol@gmail.com>
2025-07-23 09:44:26 +0200

    Revert linting fixes

--- src/utils/ts-internal.ts
+++ src/utils/ts-internal.ts
@@ -2,7 +2,7 @@ import * as ts from 'typescript';
 const tsany = ts as any;
 
 // https://github.com/Microsoft/TypeScript/blob/v2.1.4/src/compiler/utilities.ts#L152
-export function getSourceFileOfNode(node: ts.Node)): ts.SourceFile {
+export function getSourceFileOfNode(node: ts.Node): ts.SourceFile {
     return tsany.getSourceFileOfNode.apply(this, arguments);
 }
 
