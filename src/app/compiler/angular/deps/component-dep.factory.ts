08238af0c56548b8e690dc30329aa55717b01218
tcorral <amischol@gmail.com>
2025-07-23 09:02:09 +0200

    Revert linting fixes

--- src/app/compiler/angular/deps/component-dep.factory.ts
+++ src/app/compiler/angular/deps/component-dep.factory.ts
@@ -89,12 +89,10 @@ export class ComponentDepFactory {
             componentDep.accessors = IO.accessors;
         }
         if (IO.properties) {
-            const { inputSignals, outputSignals, properties } = this.helper.getInputOutputSignals(
-                IO.properties
-            );
+            const {inputSignals, outputSignals, properties} = this.helper.getInputOutputSignals(IO.properties);
 
-            componentDep.inputsClass = componentDep.inputsClass.concat(inputSignals);
-            componentDep.outputsClass = componentDep.outputsClass.concat(outputSignals);
+            componentDep.inputsClass = componentDep.inputsClass.concat(inputSignals)
+            componentDep.outputsClass = componentDep.outputsClass.concat(outputSignals)
             componentDep.propertiesClass = properties;
         }
 
