bc564eef7988720354313846796588cae6238054
tcorral <amischol@gmail.com>
2025-07-23 09:42:57 +0200

    Revert linting fixes

--- src/utils/router-parser.util.ts
+++ src/utils/router-parser.util.ts
@@ -813,7 +813,7 @@ export class RouterParserUtil {
                                     propertyInitializer.kind === SyntaxKind.PropertyAccessExpression
                                 ) {
                                     let lastObjectLiteralAttributeName =
-                                        propertyInitializer.name.getText(),
+                                            propertyInitializer.name.getText(),
                                         firstObjectLiteralAttributeName;
                                     if (propertyInitializer.expression) {
                                         firstObjectLiteralAttributeName =
