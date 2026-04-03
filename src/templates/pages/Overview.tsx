fc258d48b889126b71c08de6790eada868b7fb78
Christian Weiss <eiweiss@users.noreply.github.com>
2026-04-03 13:05:00 +0200

    feat: modernize overview page with standalone/token counts and app config
    Add interceptors counter, app config card linking to app-config.html,
    standalone count subtitles on component/directive cards, token count on
    injectables card. Hide module graph for standalone apps (no modules).

--- src/templates/pages/Overview.tsx
+++ src/templates/pages/Overview.tsx
@@ -7,6 +7,7 @@ type OverviewCard = {
     readonly singularKey: string;
     readonly pluralKey: string;
     readonly href?: string;
+    readonly subtitle?: string;
 };
 
 type OverviewProps = {
@@ -15,6 +16,7 @@ type OverviewProps = {
     readonly entities?: unknown[];
     readonly directives?: unknown[];
     readonly injectables?: unknown[];
+    readonly interceptors?: unknown[];
     readonly pipes?: unknown[];
     readonly classes?: unknown[];
     readonly guards?: unknown[];
@@ -24,6 +26,7 @@ type OverviewProps = {
     readonly mainGraph?: string;
     readonly disableGraph?: boolean;
     readonly disableMainGraph?: boolean;
+    readonly appConfig?: any[];
 };
 
 const Card = (props: OverviewCard): string => {
@@ -38,6 +41,7 @@ const Card = (props: OverviewCard): string => {
                 <div class="card-block">
                     <h4 class="card-title"><span class={`icon ${props.icon}`}></span></h4>
                     <p class="card-text">{content}</p>
+                    {props.subtitle && <p class="card-text"><small>{props.subtitle}</small></p>}
                 </div>
             </div>
         </div>
@@ -47,16 +51,29 @@ const Card = (props: OverviewCard): string => {
 export const Overview = (props: OverviewProps): string => {
     const cards: OverviewCard[] = [];
 
+    // App config card
+    if (props.appConfig?.length)
+        cards.push({ icon: 'ion-ios-settings', count: props.appConfig.length, singularKey: 'configuration', pluralKey: 'configurations', href: './app-config.html', subtitle: 'ApplicationConfig' });
+
     if (props.modules?.length)
         cards.push({ icon: 'ion-ios-archive', count: props.modules.length, singularKey: 'module', pluralKey: 'modules', href: './modules.html' });
-    if (props.components?.length)
-        cards.push({ icon: 'ion-md-cog', count: props.components.length, singularKey: 'component', pluralKey: 'components' });
+
+    if (props.components?.length) {
+        const standalone = (props.components as any[]).filter(c => c.standalone).length;
+        cards.push({ icon: 'ion-md-cog', count: props.components.length, singularKey: 'component', pluralKey: 'components', subtitle: standalone > 0 ? `${standalone} standalone` : undefined });
+    }
     if (props.entities?.length)
         cards.push({ icon: 'ion-md-swap', count: props.entities.length, singularKey: 'entities', pluralKey: 'entities' });
-    if (props.directives?.length)
-        cards.push({ icon: 'ion-md-code-working', count: props.directives.length, singularKey: 'directive', pluralKey: 'directives' });
-    if (props.injectables?.length)
-        cards.push({ icon: 'ion-md-arrow-round-down', count: props.injectables.length, singularKey: 'injectable', pluralKey: 'injectables' });
+    if (props.directives?.length) {
+        const standalone = (props.directives as any[]).filter(d => d.standalone).length;
+        cards.push({ icon: 'ion-md-code-working', count: props.directives.length, singularKey: 'directive', pluralKey: 'directives', subtitle: standalone > 0 ? `${standalone} standalone` : undefined });
+    }
+    if (props.injectables?.length) {
+        const tokens = (props.injectables as any[]).filter(i => i.isToken).length;
+        cards.push({ icon: 'ion-md-arrow-round-down', count: props.injectables.length, singularKey: 'injectable', pluralKey: 'injectables', subtitle: tokens > 0 ? `${tokens} tokens` : undefined });
+    }
+    if (props.interceptors?.length)
+        cards.push({ icon: 'ion-ios-swap', count: props.interceptors.length, singularKey: 'interceptor', pluralKey: 'interceptors' });
     if (props.pipes?.length)
         cards.push({ icon: 'ion-md-add', count: props.pipes.length, singularKey: 'pipe', pluralKey: 'pipes' });
     if (props.classes?.length)
@@ -68,11 +85,13 @@ export const Overview = (props: OverviewProps): string => {
     if (props.routes?.length)
         cards.push({ icon: 'ion-ios-git-branch', count: props.routesLength ?? 0, singularKey: 'route', pluralKey: 'routes', href: './routes.html' });
 
+    const hasModules = props.modules?.length > 0;
+
     return (<>
         <ol class="breadcrumb">
             <li class="breadcrumb-item">{t('overview')}</li>
         </ol>
-        {!props.disableGraph && !props.disableMainGraph && props.modules && (
+        {!props.disableGraph && !props.disableMainGraph && hasModules && (
             <div class="text-center module-graph-container">
                 <div id="module-graph-svg">
                     {props.mainGraph}
@@ -90,7 +109,5 @@ export const Overview = (props: OverviewProps): string => {
                 {cards.map(card => Card(card))}
             </div>
         </div>
-        {!props.disableGraph && (<>
-        </>)}
     </>) as string;
 };
